677c3a619c2c6ac7406be911f22d08f4fc7b0280
agrochal <arturgrochal2805@gmail.com>
2019-12-23 19:04:15 +0100

    Init commit

---
+++ src/Plugin/Block/RollTheDiceBlock.php
@@ -0,0 +1,31 @@
+<?php
+namespace Drupal\roll_the_dice\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+/**
+ * Dice roller
+ *
+ * @Block(
+ *   id = "RollTheDiceBlock",
+ *   admin_label = @Translation("Javascript dice roller"),
+ * )
+ */
+class RollTheDiceBlock extends BlockBase {
+  /**
+  * {@inheritdoc}
+  */
+  public function build() {
+    return [
+      '#markup' => '<div id="dice"></div><div id="btn-box"><div id="button">Roll the dice!</div></div>',
+      '#attached' => [
+        'library' => [
+          'roll_the_dice/scripts',
+          'roll_the_dice/styles'
+        ],
+      ],
+    ];
+  }
+
+}
