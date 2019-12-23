1b80b8609019202e128541423dc222b8011b220f
agrochal <arturgrochal2805@gmail.com>
2019-12-23 19:19:42 +0100

    Fix coding standards

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+/**
+ * @file
+ */
+
 (function ($, Drupal) {
   var dice = document.getElementById('dice');
-  for(var i=0;i<3;i++){
+  for (var i = 0; i < 3; i++) {
     var row = document.createElement("div");
     row.className = "row";
-    for(var j=0;j<3;j++){
+    for (var j = 0; j < 3; j++) {
       var box = document.createElement("div");
       box.className = "box";
       var dot = document.createElement("div");
@@ -18,38 +22,44 @@
 var dots = document.getElementsByClassName('dot');
 var stay = [];
 function change(value) {
-  switch(value){
+  switch (value) {
   case 1:
     stay = [4];
     break;
+
   case 2:
     stay = [2,6];
     break;
+
   case 3:
      stay = [2,4,6];
     break;
+
   case 4:
     stay = [0,2,6,8];
     break;
+
   case 5:
     stay = [0,2,4,6,8];
     break;
+
   case 6:
     stay = [0,2,3,5,6,8];
     break;
 }
-  for(var i=0;i<dots.length;i++){
-    if(stay.includes(i)){
+  for (var i = 0; i < dots.length; i++) {
+    if (stay.includes(i)) {
       dots[i].style.visibility = "visible";
-    } else {
+    }
+else {
       dots[i].style.visibility = "hidden";
     }
   }
 }
 
 var button = document.getElementById('button');
-button.addEventListener("click", function(){
-  for(var i=0;i<6;i++){
-    setTimeout(function(){ change(Math.floor(Math.random() * 6) + 1); }, 250*(i+1));
+button.addEventListener("click", function () {
+  for (var i = 0; i < 6; i++) {
+    setTimeout(function () { change(Math.floor(Math.random() * 6) + 1); }, 250 * (i + 1));
   }
 });
--- src/Plugin/Block/RollTheDiceBlock.php
+++ src/Plugin/Block/RollTheDiceBlock.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace Drupal\roll_the_dice\Plugin\Block;
+
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\Access\AccessResult;
+
 /**
- * Dice roller
+ * Dice roller.
  *
  * @Block(
  *   id = "RollTheDiceBlock",
@@ -13,16 +13,17 @@ use Drupal\Core\Access\AccessResult;
  * )
  */
 class RollTheDiceBlock extends BlockBase {
+
   /**
-  * {@inheritdoc}
-  */
+   * {@inheritdoc}
+   */
   public function build() {
     return [
       '#markup' => '<div id="dice"></div><div id="btn-box"><div id="button">Roll the dice!</div></div>',
       '#attached' => [
         'library' => [
           'roll_the_dice/scripts',
-          'roll_the_dice/styles'
+          'roll_the_dice/styles',
         ],
       ],
     ];
